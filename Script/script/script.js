d74a7d48a6c9cf8bb131d9aa521216c80d5f06cd
jcooper98 <109708356+jcooper98@users.noreply.github.com>
2022-08-22 10:48:21 -0700

    Banner Change

--- Script/script/script.js
+++ Script/script/script.js
@@ -1,23 +1,81 @@
-//add dark navbar when scrolling//
-
-const header = document.querySelector('.navbar');
-
-window.onscroll = function()
-{
-    var top = window.scrollY;
-    if(top >=100)
-    {
-        header.classList.add('navbarDark');
-    }
-    else
-    {
-        header.classList.remove('navbarDark');
-    }
-}
-
 //Detect if user is dark mode 
 
 const isDarkMode = () =>
 {
 return window.matchMedia('(prefers-color-scheme: dark').matches;
 }   
+
+/*
+https://github.com/daybrush/scenejs
+*/
+const scene = new Scene({
+    ".container": {}
+  }, {
+    selector: true,
+  });
+  
+  const item = scene.getItem(".container");
+  
+  
+  function move(startTime, endTime, left, top, rotate, scale) {
+    item.set({
+      [`${startTime}, ${endTime}`]: Scene.kineticFrame({
+        left: `${left}px`,
+        top: `${top}px`
+      }).set({
+        transform: {
+          rotate: `${rotate}deg`,
+          scale,
+        }
+      }),
+    });
+  }
+  
+  
+  
+  move(0, 0, 90, 115, 0, 5);
+  move(1, 1, 90, 115, 0, 2);
+  move(2, 3, 0, 115, 0, 1);
+  move(4, 4.5, -100, 0, -90, 2);
+  move(5.5, 6, -52, -18, -90, 1.6);
+  move(7, 7.5, 30, 45, 0, 2);
+  move(8.5, 9,  10, 30, 0, 3);
+  move(10, 10.5, 28, 0, 0, 2.2);
+  move(11.5, 12, 50, -35, 0, 1.65);
+  move(13, 13.5, 35, -70, 0, 2);
+  move(14.5, 18, 0, 0, 0, 1);
+  
+  
+  /*
+  typing
+  https://github.com/daybrush/scenejs-effects
+  */
+  scene.set({
+    "[data-typing='i']": Scene.typing({ text: "I ", duration: 1}),
+    "[data-typing='frontend']": {
+      1: Scene.typing({ text: "'m Front-End", duration: 1}),
+    },
+    "[data-typing='aspiring']": {
+      1.5: Scene.typing({ text: "aspriring", duration: 1}),
+    },
+    "[data-typing='fullstack']": {
+      3.3: Scene.typing({ text: "Full-stack", duration: 0.5}),
+    },
+    "[data-typing='engineer']": {
+      4.5: Scene.typing({ text: "Engineer", duration: 1}),
+    },
+    "[data-typing='in']": {
+      6: Scene.typing({ text: "in", duration: 1}),
+    },
+    "[data-typing='sacramento']": {
+      7.5: Scene.typing({ text: "Sacramento", duration: 0.7}),
+    },
+  });
+  
+  scene.setPlaySpeed(1);
+  scene.setEasing("ease-in-out");
+  scene.setIterationCount("infinite");
+  scene.play();
+  
+  
+  // new Timeline(scene, document.body)
